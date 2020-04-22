0ac39796c0ae65da0c2048c0c77e246466a7a486
Elizabeth Gallmeister <elizabethgallmeister@gmail.com>
2020-04-21 21:59:24 -0700

    added to game while loop

--- hw1withxcoderepo/siete-y-medio.cpp
+++ hw1withxcoderepo/siete-y-medio.cpp
@@ -8,29 +8,31 @@
 using namespace std;
 
 // Global constants (if any)
-
-
 // Non member functions declarations (if any)
-
-
+void drawcards(){};
 // Non member functions implementations (if any)
 
 
 // Stub for main
 int main(){
-   int availableMoney = 100;
-   int bet;
-   cout<<"working"<<endl;
-   bool done = false;
-   while(!done)
+    Player* person = new Player(100); //initialize person with $100
+    Hand personhand(); //lol is this ok
+    Hand dealerhand(); //^^^uhhh
+    int bet;
+    bool done = false;
+    while(!done)
    {
-       cout<<"You have $"<< availableMoney << ". Enter bet: ";
+       cout<<"You have $"<< person->get_money() << ". Enter bet: ";
        cin>>bet;
-       if(bet > availableMoney)
+       if(bet > person->get_money())
        {
            cout<< "You don't have that much. Enter bet: ";
+           cin>>bet;
            cout<< endl;
        }
+       drawcards();
+       cout<< "Your cards: \n";
+       //cout<< "\t " << get_spanish_rank() << " of " << get_spanish_suit();
        done = true;
    }
    return 0;
